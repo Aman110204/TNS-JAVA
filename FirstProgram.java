e544602ca7d53a519a0f5071a1208f6278bc671f
Aman110204 <amankumar110204@gmail.com>
2025-11-19 18:45:02 +0530

    Update FirstProgram.java

--- FirstProgram.java
+++ FirstProgram.java
@@ -1,10 +1,9 @@
-// Modified by Aman
 package skit_codes;
 
 public class FirstProgram {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		System.out.println("Hello World,By Aman");
 
 	}
 
